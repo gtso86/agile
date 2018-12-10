212852d7940331da5cfbd20e395ff8c982628a89
Gabriel Tadeu <contato@gabrieltadeu.com>
2018-12-10 12:15:43 -0200

    678b267

---
+++ precache-manifest.17fda97d0cc161e6fec934bddd2b1f49.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/agile/robots.txt"
+  },
+  {
+    "revision": "bb9a1ccbb2aaf756e8d8",
+    "url": "/agile/js/chunk-vendors.0e357257.js"
+  },
+  {
+    "revision": "d096d4b8d6207945e5b2",
+    "url": "/agile/js/app.8ff585c1.js"
+  },
+  {
+    "revision": "3090734173a961b7e2ab",
+    "url": "/agile/js/about.2b5fddbd.js"
+  },
+  {
+    "revision": "b0c914d7fbbeb772a4798d8150a4fbff",
+    "url": "/agile/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/agile/img/logo.82b9c7a5.png"
+  }
+];
